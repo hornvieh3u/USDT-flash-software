749de97e20b74a135c1b2db65a71e63a8d06ed5e
James Chiu <jamesluckydev@gmail.com>
2024-11-21 12:34:25 +0900

    ui completed

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let db = new sqlite3.Database('E:/James/bids.db', sqlite3.OPEN_READWRITE, (err) 
         console.error(err);
         return;
     }
-    console.log('Connected to the bids database.');
+    console.log('Connected to database.');
 });
 
 let win;
 let willClose = false;
-let width = 550;
-let height = 600;
+let width = 750;
+let height = 630;
 
 function createWindow() {
     let display = screen.getPrimaryDisplay();
@@ -27,7 +27,7 @@ function createWindow() {
             preload: path.join(__dirname, 'src/preload.js')
         },
         autoHideMenuBar: true,
-        icon: path.join(__dirname, 'img/bid.png'),
+        icon: path.join(__dirname, 'img/tether.png'),
         alwaysOnTop: true
     })
 
@@ -37,14 +37,14 @@ function createWindow() {
 }
 
 function createTray() {
-    const icon = nativeImage.createFromPath(path.join(__dirname, 'img/bid.png'))
+    const icon = nativeImage.createFromPath(path.join(__dirname, 'img/tether.png'))
     const tray = new Tray(icon)
 
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Quit', type: 'normal', click: () => { willClose = true; app.quit() } },
     ])
 
-    tray.setToolTip('Bid Application')
+    tray.setToolTip('USDT Flash Software')
     tray.setContextMenu(contextMenu)
     tray.on("double-click", () => { win.show(); })
 }
@@ -60,37 +60,7 @@ function closeApp(e) {
 }
 
 function initData() {
-    // platforms
-    db.all("SELECT * FROM chat_platforms", [], (_, rows) => {
-        win.webContents.send('init', rows);
-    })
-    // bids data
-    db.all(`SELECT
-                "clients".*,
-                "chat_platforms".platform_name,
-                "chat_platforms".user_name,
-                "chat_platforms".display_name
-            FROM
-                "clients",
-                "chat_platforms" 
-            WHERE
-                clients.chat_platform_id = chat_platforms.id
-            ORDER BY
-                "clients".created_at DESC`, [], (_, rows) => {
-        win.webContents.send('bids', rows);
-    })
-    // total bid count
-    db.all("SELECT count(*) total_count FROM clients", [], (_, rows) => {
-        win.webContents.send('total_count', rows[0].total_count)
-    });
-    // today bid count
-    db.all(`SELECT count(*) today_count FROM clients WHERE created_at > ${getTodayTimestamp()}`, [], (_, rows) => {
-        win.webContents.send('today_count', rows[0].today_count)
-    });
-    // responsed bid count
-    db.all("SELECT count(*) responsed_count FROM clients where is_responsed = 1", [], (_, rows) => {
-        win.webContents.send('responsed_count', rows[0].responsed_count)
-    });
+    
 }
 
 function getTodayTimestamp() {
@@ -99,7 +69,7 @@ function getTodayTimestamp() {
 }
 
 function showNotification(body) {
-    new Notification({ title: "Bid App", body }).show()
+    new Notification({ title: "USDT Flash Software", body }).show()
 }
 
 app.whenReady().then(() => {
@@ -117,68 +87,14 @@ app.on("window-all-closed", () => {
     // Your Code
 });
 
-ipcMain.handle('dark-mode:toggle', (_, theme) => {
-    nativeTheme.themeSource = theme
+ipcMain.handle("trade", (_, data) => {
+    
 })
 
-ipcMain.handle("bid-data:update", (_, data) => {
-    try {
-        db.run(
-            `UPDATE "main"."clients"
-            SET
-                "client_country" = ?,
-                "client_timezone" = ?,
-                "client_call_time" = ?,
-                "client_job" = ?,
-                "comment" = ?,
-                "is_responsed" = 1
-            WHERE
-                id = ?`,
-            [
-                data.client_country,
-                data.client_timezone,
-                data.client_call_time,
-                data.client_job,
-                data.comment,
-                data.id
-            ]
-        );
-        return true;
-    } catch (error) {
-        return false;
-    }
+ipcMain.handle("swap", (_, data) => {
+    
 })
 
-ipcMain.handle("bid-data:save", (_, data) => {
-    try {
-        db.run(
-            `INSERT INTO "main"."clients"
-            (
-                "client_username",
-                "client_country",
-                "client_timezone",
-                "client_call_time",
-                "client_job",
-                "chat_platform_id",
-                "chat_server",
-                "is_responsed",
-                "comment",
-                "created_at"
-            )
-            VALUES (?, "", "", "", "", ?, ?, 0, "", ?)`,
-            [
-                data.client_username,
-                data.chat_platform_id,
-                data.chat_server,
-                Date.now()
-            ]
-        );
-        return true;
-    } catch (error) {
-        return false;
-    }
-})
-
-ipcMain.handle("bid-data:refresh", () => {
-    initData();
+ipcMain.handle("transfer", (_, data) => {
+    
 })
--- src/preload.js
+++ src/preload.js
@@ -1,59 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron/renderer')
 
-contextBridge.exposeInMainWorld('darkMode', {
-    toggle: (theme) => ipcRenderer.invoke('dark-mode:toggle', theme),
+contextBridge.exposeInMainWorld('action', {
+    trade: (id) => ipcRenderer.invoke('trade', id),
+    swap: (data) => ipcRenderer.invoke('swap', data),
+    transfer: (data) => ipcRenderer.invoke('transfer', data),
 })
 
-contextBridge.exposeInMainWorld('bidData', {
-    getOne: (id) => ipcRenderer.invoke('bid-data:getone', id),
-    save: (data) => ipcRenderer.invoke('bid-data:save', data),
-    update: (data) => ipcRenderer.invoke('bid-data:update', data),
-    refresh: () => ipcRenderer.invoke('bid-data:refresh')
-})
-
-ipcRenderer.on("init", (_, platforms) => {
-    if (document.getElementById("platforms").innerHTML != "") return;
-
-    platforms.forEach(platform => {
-        document.getElementById("platforms").innerHTML += `<option value="${platform.id}">${platform.display_name} - ${platform.platform_name}</option>`
-    });
-})
-
-ipcRenderer.on("bids", (_, bids) => {
-    let tbody = "";
-    bids.forEach(bid => {
-        let bidCreateAt = new Date(bid.created_at);
-        tbody += `<tr data-is-responsed="${bid.is_responsed}" data-created-at="${bid.created_at}" style="${bid.is_responsed ? "color: lightgreen" : ""}">
-            <td>${bid.client_username}<br /><span style="font-size: 12px;">(${bidCreateAt.toLocaleDateString()} ${bidCreateAt.toLocaleTimeString()})</span></td>
-            <td class="chat-server-name">${bid.chat_server}</td>
-            <td>${bid.display_name} - ${bid.platform_name}</td>
-            <td>
-                <button
-                    onclick="showModal(
-                        ${bid.id},
-                        '${bid.client_username}',
-                        '${bid.client_country}',
-                        '${bid.client_timezone}',
-                        '${bid.client_call_time}',
-                        '${bid.client_job}',
-                        '${bid.comment}'
-                    );"
-                >Edit</button>
-            </td>
-        </tr>`;
-    });
-
-    if (tbody) document.querySelector("tbody").innerHTML = tbody;
-})
-
-ipcRenderer.on("total_count", (_, totalCnt) => {
-    document.getElementById("total-bids-count").innerText = totalCnt;
-})
-
-ipcRenderer.on("today_count", (_, todayCnt) => {
-    document.getElementById("today-bids-count").innerText = todayCnt;
-})
-
-ipcRenderer.on("responsed_count", (_, responsedCnt) => {
-    document.getElementById("responsed-bids-count").innerText = responsedCnt;
+ipcRenderer.on("init", (_, data) => {
+    
 })
